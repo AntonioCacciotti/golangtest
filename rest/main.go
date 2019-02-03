b11149e10b3b94d06a1bed8255404e1f485b8f49
Antonio <antonio.cacciotti@gmail.com>
2019-02-03 16:00:11 +0100

    fix handler in Get verb

--- rest/main.go
+++ rest/main.go
@@ -10,12 +10,16 @@ import (
 
 func main() {
 	router := gin.Default()
-	router.GET("/v1/:game/:action", func(c *gin.Context) {
-		game := c.Param("game")
-		action := c.Param("action")
-		c.String(http.StatusOK, "ciao %s %s", game, action)
-		log.Println(action)
-		model.LoadQuestion()
-	})
+	router.GET("/v1/:game/:action", GetQuestions)
+	router.POST("v1/:game/registeruser")
 	router.Run(":8000")
+}
+
+//GetQuestions reads all questions from a file
+func GetQuestions(c *gin.Context) {
+	game := c.Param("game")
+	action := c.Param("action")
+	c.String(http.StatusOK, "ciao %s %s", game, action)
+	log.Println(action)
+	model.LoadQuestion()
 }
